b19b236474af676d5a52bb6fdd6a244fbf5a052b
Mason <mason.seward49@ed.cna.nl.ca>
2022-08-07 21:37:39 -0230

    Improvements to the CreateWord class. A word will no longer be repeated until all words have been used.

--- src/cp2561_project/CreateWord.java
+++ src/cp2561_project/CreateWord.java
@@ -2,6 +2,7 @@ package cp2561_project;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CreateWord
@@ -17,29 +18,60 @@ public class CreateWord
         int i = 0;
         String word = "";
         String fileName = "hangman_words.txt";
-        try {
+        ArrayList<String> usedWords = new ArrayList<>();
+        boolean duplicateWord = false;
+
+        try
+        {
             Scanner readFile = new Scanner(new File(fileName));
 
 
-            int random_int = (int) Math.floor(Math.random() * (200 - 1 + 1) + 1);
+            int random_int = generateInt();
 
             while (readFile.hasNext())
             {
                 i++;
                 word = readFile.next();
+                
+                if (usedWords.contains(readFile.next()))
+                {
+                    duplicateWord = true;
+                }
+                
                 if (i == random_int)
                 {
-                    word = readFile.next();
-                    break;
+                    if (!duplicateWord)
+                    {
+                        word = readFile.next();
+                        usedWords.add(word);
+                        break;
+                    }
+
+                    if (duplicateWord)
+                    {
+                        random_int = generateInt();
+                    }
                 }
             }
+
+            if (usedWords.size() == 50)
+            {
+                usedWords.clear();
+            }
+
             readFile.close();
             return word;
-            }
+        }
+
         catch (FileNotFoundException e)
         {
             System.out.println("The file could not be found. " + e);
         }
         return word;
+    }
+
+    public static int generateInt()
+    {
+        return (int) Math.floor(Math.random() * (200 - 1 + 1) + 1);
     }
 }
