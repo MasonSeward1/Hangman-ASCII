c9409be6f9b1f322b51fb6a6f0fbaaa4a2cc4e32
Mason <mason.seward49@ed.cna.nl.ca>
2022-08-10 11:41:31 -0230

    Fix for CreateWord not generating a random word.

--- src/cp2561_project/CreateWord.java
+++ src/cp2561_project/CreateWord.java
@@ -3,6 +3,7 @@ package cp2561_project;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class CreateWord
@@ -10,68 +11,39 @@ public class CreateWord
     // Used for testing the generateWord method.
 //    public static void main(String[] args)
 //    {
+//        for (int i = 0; i < 50; i++)
 //        System.out.println(generateWord());
 //    }
 
     public static String generateWord()
     {
-        int i = 0;
-        String word = "";
         String fileName = "hangman_words.txt";
+        ArrayList<String> words = new ArrayList<>();
         ArrayList<String> usedWords = new ArrayList<>();
-        boolean duplicateWord = false;
 
         try
         {
             Scanner readFile = new Scanner(new File(fileName));
 
-
-            int random_int = generateInt();
-
             while (readFile.hasNext())
             {
-                i++;
-                word = readFile.next();
-                
-                if (usedWords.contains(readFile.next()))
-                {
-                    duplicateWord = true;
-                }
-                
-                if (i == random_int)
-                {
-                    if (!duplicateWord)
-                    {
-                        word = readFile.next();
-                        usedWords.add(word);
-                        break;
-                    }
-
-                    if (duplicateWord)
-                    {
-                        random_int = generateInt();
-                    }
-                }
+                words.add(readFile.nextLine());
             }
-
-            if (usedWords.size() == 50)
-            {
-                usedWords.clear();
-            }
-
-            readFile.close();
-            return word;
         }
 
         catch (FileNotFoundException e)
         {
             System.out.println("The file could not be found. " + e);
         }
-        return word;
-    }
 
-    public static int generateInt()
-    {
-        return (int) Math.floor(Math.random() * (200 - 1 + 1) + 1);
+        Collections.shuffle(words);
+        usedWords.add(words.toString());
+
+        if (usedWords.contains(words.toString()))
+        {
+            Collections.shuffle(words);
+        }
+
+        return words.get(0);
     }
 }
